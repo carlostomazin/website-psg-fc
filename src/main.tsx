475a1af333338d9509cf735882b25eaaef3d259f
carlostomazin <carlos.tomazin2@gmail.com>
2026-03-26 00:12:45 -0300

    feat: add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+import './index.css'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
-import './index.css'
+
 import { ThemeProvider } from "@/components/theme-provider"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Set up a Router instance
 const router = createRouter({
@@ -19,6 +21,8 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient()
+
 // Render the app
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
@@ -26,7 +30,9 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </StrictMode>,
   )
--- src/routes/_app/games/index.tsx
+++ src/routes/_app/games/index.tsx
@@ -1,13 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/_app/games/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  // Mock data with React Query
+  const { data } = useQuery({
+    queryKey: ['games'],
+    queryFn: async () => {
+    // Simulate an API call
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return ['Game 1', 'Game 2', 'Game 3']
+  },})
+
   return (
     <>
     <div>Hello "/games/"!</div>
+    <div>Games: {data ? data.join(', ') : 'Loading...'}</div>
     </>
   )
 }
